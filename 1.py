eed4abf64a268a719cecdb9290cff21bd2171085
ytka00 <120676215+ytka00@users.noreply.github.com>
2023-07-15 11:26:23 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,34 +2,34 @@ import string
 allowed = set(string.ascii_lowercase+' ')
 charstr = 'abcdefghijklmnopqrstuvwxyz '
 
+def charfind(egyes,kettes,jel):
+    megoldas= ''
+    for a in range(len(egyes):
+        j=0
+        z=0
+        while egyes[a]!=charstr[j]:
+            j+=1
+        while kettes[a]!=charstr[z]:
+            z+=1
+        if jel=='-': 
+            megoldas+=charstr[(z-j)%27]
+        else:
+            megoldas+=charstr[(j+z)%27]
+    return(megoldas)
 def rejtjelezes():
     uzenet = str(input('Adja meg az üzenetet! '))
     key = str(input('Adja meg a kulcsot! '))
     cipher = ''
-    if all(ch in allowed for ch in uzenet)==False:
-        print('Az üzenet hibás karaktereket tartalmaz')
-        exit(1)
-    if all(ch in allowed for ch in key)==False:
-        print('A kulcs hibás karaktereket tartalmaz')
+    if all((ch in allowed for ch) in uzenet or key)==False:
+        print('Az üzenet vagy kulcs hibás karaktereket tartalmaz')
         exit(1)
     if len(uzenet)>len(key):
         print('Hibás adatokat adott meg')
         exit(1)
-    j = 0
-    z = 0
-    for i in range (len(uzenet)):
-        while uzenet[i]!=charstr[j]:
-            j+=1
-        while key[i]!=charstr[z]:
-            z+=1
-        cipher+=charstr[(j+z)%27]
-        z = 0
-        j = 0
+    
+    cipher=charfind(uzenet,key,'')
     print(cipher)
 def megfejtes():
-    z=0
-    j=0
-    uzenet=''
     cipher=input('Adja meg a kódot: ')
     key = input('Adja meg a kulcsot: ')
     if len(key)<len(cipher):
@@ -41,14 +41,8 @@ def megfejtes():
     if all(ch in allowed for ch in key)==False:
         print('A kulcs hibás karaktereket tartalmaz')
         exit(1)
-    for i in range (len(cipher)):
-        while  cipher[i]!=charstr[j]:
-            j+=1
-        while key[i]!=charstr[z]:
-            z+=1
-        uzenet+=charstr[(j-z)%27]
-        z=0
-        j=0
+   
+    uzenet=charfind(cipher,key,'-')
     print(uzenet)
 def main():
     a = input('1: titkosítás \n2: deciphering\n')
